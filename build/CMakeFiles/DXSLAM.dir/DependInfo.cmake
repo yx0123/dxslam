c713e1fb3e13a758961ed3939c4f8defff97fbfb
Yeoh Yu Xuan <yyx60-khs@yahoo.com.sg>
2020-11-12 12:44:46 +0800

    first commit

---
+++ build/CMakeFiles/DXSLAM.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Converter.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Converter.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Frame.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Frame.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/FrameDrawer.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/FrameDrawer.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Initializer.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Initializer.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/KeyFrame.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/KeyFrame.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/KeyFrameDatabase.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/KeyFrameDatabase.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/LocalMapping.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/LocalMapping.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/LoopClosing.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/LoopClosing.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Map.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Map.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/MapDrawer.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/MapDrawer.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/MapPoint.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/MapPoint.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Matcher.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Matcher.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Optimizer.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Optimizer.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/PnPsolver.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/PnPsolver.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Sim3Solver.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Sim3Solver.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/System.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/System.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Tracking.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Tracking.cc.o"
+  "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/src/Viewer.cc" "/home/dh/workspaces/dxslam_ws/src/dxslam_ros/dxslam/build/CMakeFiles/DXSLAM.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/dh/Pangolin/include"
+  "/home/dh/Pangolin/build/src/include"
+  "../Thirdparty/cnpy"
+  "../Thirdparty/fbow/src"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/DXSLAM.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DXSLAM.dir/src/System.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Tracking.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/LocalMapping.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/LoopClosing.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Matcher.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/FrameDrawer.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Converter.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/MapPoint.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/KeyFrame.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Map.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/MapDrawer.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Optimizer.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/PnPsolver.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Frame.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/KeyFrameDatabase.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Sim3Solver.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Initializer.cc.o"
+  "CMakeFiles/DXSLAM.dir/src/Viewer.cc.o"
+  "../lib/libDXSLAM.pdb"
+  "../lib/libDXSLAM.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DXSLAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
